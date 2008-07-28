15831ce3df7112676f41dd95cb970ec28d691818
Andriy Bazyuta <tihhra@gmail.com>
2008-07-28 16:11:06 +0300

    talk repair

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,7 +3,7 @@ belongs_to :user
 has_many :comments, :as => :commentable, :dependent => :destroy 
 has_many :user_subscribes, :as => :subscribtable
 validates_presence_of :title, :content
-named_scope :active_only, :conditions=>{:is_active=>true},  :order => "talks.priority DESC, talks.created_at ASC, talks.last_commented DESC", :include => [:user]
+named_scope :active_only, :conditions=>{:is_active=>true},  :order => "talks.priority DESC, talks.last_commented DESC, talks.created_at ASC", :include => [:user]
 
 
   acts_as_postable_content
