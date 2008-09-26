fa604b085f3d3dc6c4f6732ca304c35c180bb15a
Andriy Bazyuta <tihhra@gmail.com>
2008-09-26 15:55:43 +0300

    whitelist

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,3 +1,8 @@
 module CommentsHelper
+    require 'hpricot'
     WhiteListHelper.tags += %w(object param embed)
+    WhiteListHelper.tags += %w(table tbody tr th td)
+    WhiteListHelper.tags += %w(font)
+    WhiteListHelper.tags += %w(span u)
+    WhiteListHelper.attributes.merge %w(id style flashvars wmode codebase classid value name type allowfullscreen)
 end
